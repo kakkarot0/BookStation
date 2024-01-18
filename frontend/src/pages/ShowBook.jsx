273c9b6ce9d35dbf70155903e3c921675fd391b2
Arun Gound <arun00gound@gmail.com>
2024-01-18 13:02:59 +0530

    [link update]

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -12,7 +12,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5000/books/${id}`)
+      .get(`https://bookstation.onrender.com/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
